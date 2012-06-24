39761267cfae0fa0af74a91c0adb4ddd209ca1e9
Emiliano Burgos <burgosemi@gmail.com>
2012-06-24 09:45:52 -0400

    Merging sencha

--- phonegap/android/HateMap/assets/www/app/view/Main.js
+++ phonegap/android/HateMap/assets/www/app/view/Main.js
@@ -14,7 +14,7 @@ Ext.define('app.view.Main', {
         navigationBar: {
             //ui: 'sencha',
             items: [
-                {
+                /*{
                     xtype: 'button',
                     id: 'hateButton',
                     text: 'Hate',
@@ -28,7 +28,7 @@ Ext.define('app.view.Main', {
                         type: 'fadeIn',
                         duration: 200
                     }
-                },
+                },*/
                 {
                     xtype: 'button',
                     id: 'saveButton',
@@ -67,7 +67,7 @@ Ext.define('app.view.Main', {
                         { iconMask: true, id:'mapButton', iconCls: 'icon-map', data:'hateMap', disabled:true },
                         { iconMask: true, id:'listButton', iconCls:'icon-list',data:'hateList' }
                     ]},
-                    { iconMask: true, id:'mapButton', iconCls: 'icon-map', data:'hateMap', disabled:true },
+                    { iconMask: true, id:'hateButton', iconCls: 'icon-map', data:'hateMap', disabled:true },
                     { xtype: 'segmentedbutton', items: [
                         { iconMask: true, id:'privateButton', iconCls: 'icon-user',  data:'private' },
                         { iconMask: true, id:'globalButton',  iconCls: 'icon-users', data:'global' }
